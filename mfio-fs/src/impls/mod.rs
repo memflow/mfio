28594fd08365fcce8c658675fe2bdc774e1923be
Aurimas Blažulionis <0x60@pm.me>
2023-05-27 19:04:51 +0300

    Remove the inefficient io_uring FS implementation

--- mfio-fs/src/impls/mod.rs
+++ mfio-fs/src/impls/mod.rs
@@ -3,10 +3,6 @@ cfg_if::cfg_if! {
         // Force use thread impl if on miri
         pub mod thread;
         pub use thread::*;
-    } else if #[cfg(all(feature = "gpl", target_os = "linux"))] {
-        // rio is a GPL3 crate, thus we only enable it if specific feature flag is flipped
-        pub mod io_uring;
-        pub use io_uring::*;
     } else {
         // Fallback to thread on any unmatched cases
         pub mod thread;
